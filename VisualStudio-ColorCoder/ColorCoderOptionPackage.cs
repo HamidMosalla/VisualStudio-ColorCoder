6df118537e0d935ce390d15440fb05127d197b76
Hamid Mosalla <Xellarix@gmail.com>
2017-04-19 06:43:47 +0430

    adds visual studio extensibility project

---
+++ VisualStudio-ColorCoder/ColorCoderOptionPackage.cs
@@ -0,0 +1,65 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ColorCoderOptionPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using System.ComponentModel;
+namespace VisualStudio_ColorCoder
+{
+    public class ColorCoderOptionPageGrid : DialogPage
+    {
+        [Category("My Category")]
+        [DisplayName("My Integer Option")]
+        [Description("My integer option")]
+        public int OptionInteger { get; set; } = 256;
+    }
+
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(ColorCoderOptionPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideOptionPage(typeof(ColorCoderOptionPageGrid), "ColorCoder", "General", 0, 0, true)]
+    public sealed class ColorCoderOptionPackage : Package
+    {
+        /// <summary>
+        /// ColorCoderOptionPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "0bf71f6b-990b-49ac-809a-940c37a463f3";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorCoderOptionPackage"/> class.
+        /// </summary>
+        public ColorCoderOptionPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
