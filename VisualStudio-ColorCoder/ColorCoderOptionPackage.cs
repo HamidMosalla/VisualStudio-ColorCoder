503d5ab814097429611fc4b8373f8373eed76a7e
Hamid Mosalla <Xellarix@gmail.com>
2017-04-20 08:10:53 +0430

    adds user interface elements for colorcoder

--- VisualStudio-ColorCoder/ColorCoderOptionPackage.cs
+++ VisualStudio-ColorCoder/ColorCoderOptionPackage.cs
@@ -16,14 +16,91 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
 using System.ComponentModel;
+using System.Drawing;
+
 namespace VisualStudio_ColorCoder
 {
+    public enum Preset
+    {
+        SuperChargerStyle = 0,
+        ScottHanselmansDream = 1,
+        MoreGirlsInTechPlease = 2
+    }
     public class ColorCoderOptionPageGrid : DialogPage
     {
-        [Category("My Category")]
-        [DisplayName("My Integer Option")]
-        [Description("My integer option")]
-        public int OptionInteger { get; set; } = 256;
+        private const string PresetSubCategory = "Presets";
+
+        [Category(PresetSubCategory)]
+        [DisplayName("Preset")]
+        [Description("Select from one of the available sets of Presets")]
+        public Preset Preset { get; set; }
+
+
+        private const string ColorSubCategory = "Colors";
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Interface")]
+        public Color Interface { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Class")]
+        public Color Class { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Abstract Class")]
+        public Color AbstractClass { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Static Class")]
+        public Color StaticClass { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Struct")]
+        public Color Struct { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Enum")]
+        public Color Enum { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Enum Constant")]
+        public Color EnumConstant { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Constructor")]
+        public Color Constructor { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Attribute")]
+        public Color Attribute { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Field")]
+        public Color Field { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Namespace")]
+        public Color Namespace { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Method")]
+        public Color Method { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Static Method")]
+        public Color StaticMethod { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Extension Method")]
+        public Color ExtensionMethod { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Automatic Property")]
+        public Color AutomaticProperty { get; set; }
+
+        [Category(ColorSubCategory)]
+        [DisplayName("Parameter")]
+        public Color Parameter { get; set; }
     }
 
     [PackageRegistration(UseManagedResourcesOnly = true)]
@@ -33,33 +110,10 @@ namespace VisualStudio_ColorCoder
     [ProvideOptionPage(typeof(ColorCoderOptionPageGrid), "ColorCoder", "General", 0, 0, true)]
     public sealed class ColorCoderOptionPackage : Package
     {
-        /// <summary>
-        /// ColorCoderOptionPackage GUID string.
-        /// </summary>
         public const string PackageGuidString = "0bf71f6b-990b-49ac-809a-940c37a463f3";
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ColorCoderOptionPackage"/> class.
-        /// </summary>
-        public ColorCoderOptionPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        protected override void Initialize()
-        {
-            base.Initialize();
-        }
-
-        #endregion
+        
+        public ColorCoderOptionPackage(){ }
+      
+        protected override void Initialize(){ base.Initialize(); }
     }
 }
