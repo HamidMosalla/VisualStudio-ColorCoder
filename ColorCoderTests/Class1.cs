5c3b901f642801a08e4ec2d2da836493d4cb1bb2
Hamid Mosalla <Xellarix@gmail.com>
2017-06-09 08:55:49 +0430

    corrects the color coder test project

---
+++ ColorCoderTests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ColorCoderTests
+{
+    public class Class1
+    {
+
+        [Fact]
+        void FirstTest()
+        {
+            1.Should().Be(1);
+        }
+
+    }
+}
